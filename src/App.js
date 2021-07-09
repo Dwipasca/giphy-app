7a55c46645333ee68841ae5d00321f475b01fd54
Dwi Pasca <dpnursanti@gmail.com>
2021-07-09 20:09:48 +0800

    add h1 in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Dwi Pasca Nursanti</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
